9200392a882a3bdc06010b0df7e803b05cf8c0a0
Muhammed Enes Çelik <94288137+menesscelik@users.noreply.github.com>
2025-07-18 17:45:37 +0300

    Product Detail added

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2d9f999ffa044cb3c63be43a91aa070891ad02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59bf121beda8fbb9c526e78367b80acb1797999b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- b2bhirdanet/src/pages/Catalog/ProductDetails.tsx
+++ b2bhirdanet/src/pages/Catalog/ProductDetails.tsx
@@ -1,7 +1,54 @@
-import { Typography } from "@mui/material";
-export default function ProductDetailsPage(){
+import { CircularProgress, Divider, Grid, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { IProduct } from "../../model/IProduct";
 
-    return(
-        <Typography variant="h2">Product Details</Typography>
+export default function ProductDetailsPage() {
+
+    const { id } = useParams();
+    const [product, setProduct] = useState<IProduct | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`http://localhost:5105/api/products/${id}`)
+            .then(response => response.json())
+            .then(data => setProduct(data))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id]);
+
+    if(loading) return <CircularProgress />
+
+    if(!product) return <h5>Product not found...</h5>;
+
+    return (
+        <Grid container spacing={6}>
+            <Grid size={{ lg:4, md:5, sm:6, xs:12}}>
+                <img         src={`http://localhost:5105/images/${product.imageUrl}`} style={{width: "100%"}} />
+            </Grid>
+            <Grid size={{lg:4, md:5, sm:6, xs:12}}>
+                <Typography variant="h3"> {product.name}</Typography>
+                <Divider sx={{mb:2}} />
+                <Typography variant="h4" color="secondary">{product.price} </Typography>
+                <TableContainer>
+                    <table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell> Name</TableCell>
+                                <TableCell>{product.name} </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell> Description</TableCell>
+                                <TableCell>{product.description} </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell> Stock</TableCell>
+                                <TableCell>{product.stock} </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </table>
+                </TableContainer>
+            </Grid>
+        </Grid>
     );
 }
