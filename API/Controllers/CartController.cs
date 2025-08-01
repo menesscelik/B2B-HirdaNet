8b6459594c89a263fc6acb5e571c505272c24d91
Muhammed Enes Çelik <94288137+menesscelik@users.noreply.github.com>
2025-08-01 11:35:49 +0300

    Shopping Cart Added

--- API/Controllers/CartController.cs
+++ API/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.DTO;
 using API.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -6,19 +7,56 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
-
+[Route("/api/[controller]")]
 public class CartController : ControllerBase
 {
     private readonly DataContext _context;
-
     public CartController(DataContext context)
     {
         _context = context;
     }
 
     [HttpGet]
-    public async Task<ActionResult<Cart>> GetCart()
+    public async Task<ActionResult<CartDTO>> GetCart()
+    {
+        return CartToDTO(await GetOrCreate());
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddItemToCart(int productId, int quantity)
+    {
+        var cart = await GetOrCreate();
+
+        var product = await _context.Products.FirstOrDefaultAsync(i => i.Id == productId);
+
+        if (product == null)
+            return NotFound("the product is not in database");
+
+        cart.AddItem(product, quantity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result)
+            return CreatedAtAction(nameof(GetCart), CartToDTO(cart));
+
+        return BadRequest(new ProblemDetails { Title = "The product can not be added to cart" });
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteItemFromCart(int productId, int quantity)
+    {
+        var cart = await GetOrCreate();
+
+        cart.DeleteItem(productId, quantity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return Ok();
+
+        return BadRequest(new ProblemDetails { Title = "Problem removing item from the cart" });
+    }
+
+    private async Task<Cart> GetOrCreate()
     {
         var cart = await _context.Carts
                     .Include(i => i.CartItems)
@@ -27,9 +65,39 @@ public class CartController : ControllerBase
                     .FirstOrDefaultAsync();
 
         if (cart == null)
-            return NotFound();
+        {
+            var customerId = Guid.NewGuid().ToString();
+
+            var cookieOptions = new CookieOptions
+            {
+                Expires = DateTime.Now.AddMonths(1),
+                IsEssential = true
+            };
+
+            Response.Cookies.Append("customerId", customerId, cookieOptions);
+            cart = new Cart { CustomerId = customerId };
+
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();
+        }
 
         return cart;
+    }
 
+    private CartDTO CartToDTO(Cart cart)
+    {
+        return new CartDTO
+        {
+            CartId = cart.CartId,
+            CustomerId = cart.CustomerId,
+            CartItems = cart.CartItems.Select(item => new CartItemDTO
+            {
+                ProductId = item.ProductId,
+                Name = item.Product.Name,
+                Price = item.Product.Price,
+                Quantity = item.Quantity,
+                ImageUrl = item.Product.ImageUrl
+            }).ToList()
+        };
     }
 }
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45d0af3e1f839791560707574fc43a49e14b721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6753631c63acfb120f8ca85726552ecf67aaa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
